98f4b6e9314e2db5aa14e2dc75b680242a72a895
pGoGetter <97802738+pgogetter@users.noreply.github.com>
2022-01-15 19:51:04 +0100

    Next commit test

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+<main id="mainContent">
+</main>
+</body>
+</html>
