952f8194ff05e20c54d31a27ace0014eb9c0d00b
SinPi816 <SinPi816@school.lu>
2022-01-16 20:20:59 +0100

    Added CardGame as test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const loadCardGame = () => {
-  let scriptElement = document.createElement('script');
-  scriptElement.setAttribute('type','text/javascript');
-  scriptElement.setAttribute('src','js/CardGame.js');
-  document.head.appendChild(scriptElement);
+  let cardGameElement = document.createElement('script');
+  let highscoreElement = document.createElement('script');
+  let cardElement = document.createElement('script');
+
+    cardGameElement.setAttribute('type','text/javascript');
+    cardGameElement.setAttribute('src','js/CardGame.js');
+
+    highscoreElement.setAttribute('type','text/javascript');
+    highscoreElement.setAttribute('src','js/Highscore.js');
+
+    cardElement.setAttribute('type','text/javascript');
+    cardElement.setAttribute('src','js/Card.js');
+
+    document.head.appendChild(cardGameElement);
+    document.head.appendChild(highscoreElement);
+    document.head.appendChild(cardElement);
 };
 
 document.getElementById('cardGameLoadButton').addEventListener('click', function () {
